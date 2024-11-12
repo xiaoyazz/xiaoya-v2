e72055fd32ff7a6ab9fe47fb69a588dc79db4b18
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2024-11-12 15:01:23 -0500

    Update site name font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
-                        <Link to="/" className="text-2xl font-bold">
+                        <Link to="/" className="text-2xl font-bold site-name">
                             Xiaoya Zou
                         </Link>
                     </div>
